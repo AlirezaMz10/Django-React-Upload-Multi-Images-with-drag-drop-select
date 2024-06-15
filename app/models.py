a5d6c9a69541f5cff2ed1933b6643f64709c885d
alirezamz <alirezapple@gmail.com>
2024-06-15 13:47:58 +0330

    first commit

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+# PRODUCT
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = "products"
+
+# IMAGE
+class Image(models.Model):
+    link = models.ImageField(upload_to=settings.MEDIA_ROOT)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = "images"
